f5dad1c0f1f3ee555a0370c24d5ad9db2f3ac67f
Dmitriy Kuznecov <pexotaxbox@gmail.com>
2025-07-30 14:18:12 +0300

    fix

--- Assets/Scripts/Core/GameInstaller.cs
+++ Assets/Scripts/Core/GameInstaller.cs
@@ -16,7 +16,7 @@ namespace Core
         [SerializeField] private InputReader _inputReader;
 
         [Header("Character Settings")] [Space]
-        [SerializeField] private CharacterView _playerPrefab;
+        [SerializeField] private GameObject _playerPrefab;
         [SerializeField] private Transform _spawnPoint;
 
 
