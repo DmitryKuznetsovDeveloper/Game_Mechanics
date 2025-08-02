0d4b971b45ad69a0a8383ecc53168db706fa78ad
Dmitriy Kuznecov <pexotaxbox@gmail.com>
2025-08-02 13:17:55 +0300

    added level installer

---
+++ Assets/Scripts/Installers/LevelInstaller.cs
@@ -0,0 +1,17 @@
+﻿using Level;
+using UnityEngine;
+using Zenject;
+
+namespace Installers
+{
+    public class LevelInstaller : MonoInstaller
+    {
+        [SerializeField] private Renderer _backgroundMaterial;
+        [SerializeField] private float _scrollSpeedY;
+        public override void InstallBindings()
+        {
+            Container.BindInterfacesTo<LevelBackground>().FromNew().AsSingle()
+                .WithArguments(_backgroundMaterial.material, _scrollSpeedY);
+        }
+    }
+}
